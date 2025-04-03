49c01d8a3db6583a778d2dec3311c3dc4d0c911e
Frank9897 <franco.vasquez9897@gmail.com>
2025-04-03 01:03:40 -0300

    Añadiendo datos a cada Compu

--- tp2_4.c
+++ tp2_4.c
@@ -15,10 +15,22 @@ struct compu
 int main()
 {
     srand(time(NULL));
-    int velocidad_aleatoria = 1 + rand() % 3;
-    int anio_aleatorio = 2015 + rand() % (2024-2015) + 1;
-    int nucleos_aleatorio = 1 + rand() % 8 ;
-    char tipos[6][10] = {"Intel", "AMD", "Celeron", "Athlon", "Core",
-        "Pentium"};
+    
+    char tipos[6][10] = {"Intel", "AMD", "Celeron", "Athlon", "Core","Pentium"};
+
+    compu pcs[N];
+
+    for (int i = 0; i < N; i++)
+    {
+            
+        int velocidad_aleatoria = 1 + rand() % 3;
+        int anio_aleatorio = 2015 + rand() % (2024-2015) + 1;
+        int nucleos_aleatorio = 1 + rand() % 8 ;
+        pcs[i].velocidad = velocidad_aleatoria;
+        pcs[i].anio = anio_aleatorio;
+        pcs[i].cantidad_nucleos = nucleos_aleatorio;
+        pcs[i].tipo_cpu = tipos[rand()%6];
+    }
+    
     return 0;
 }
