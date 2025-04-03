f58f204ad930551fa2658732d61eb5c5a2e220f9
Frank9897 <franco.vasquez9897@gmail.com>
2025-04-03 00:02:22 -0300

    Se define una estructura

---
+++ tp2_4.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+struct compu
+{
+    int velocidad;
+    int anio;
+    int cantidad_nucleos;
+    char *tipo_cpu;
+}typedef compu;
+
+
+int main()
+{
+    return 0;
+}
